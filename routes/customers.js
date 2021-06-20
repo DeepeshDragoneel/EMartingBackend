62cbce7730b19eb894eef0ae42386bf5903cbdca
DeepeshDragoneel <deepeshash444@gmail.com>
2021-06-20 22:21:24 +0530

    Heroku testing

--- routes/customers.js
+++ routes/customers.js
@@ -3,21 +3,21 @@ const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const productController = require("../controllers/productController");
 const authController = require("../controllers/authController");
-// //middleware
-// const imageUploader = require("../middleware/upload.js");
-// const imageValidator = require("../middleware/pictureImageValidator.js");
-// const product = require("../models/product");
-// const {
-//     userSignInValidatorFunc,
-//     userSignInValidator,
-// } = require("../Validators/UserSignInValidator");
+//middleware
+const imageUploader = require("../middleware/upload.js");
+const imageValidator = require("../middleware/pictureImageValidator.js");
+const product = require("../models/product");
+const {
+    userSignInValidatorFunc,
+    userSignInValidator,
+} = require("../Validators/UserSignInValidator");
 
-// router.post(
-//     "/productImages",
-//     imageUploader,
-//     imageValidator,
-//     productController.postproductImages
-// );
+router.post(
+    "/productImages",
+    imageUploader,
+    imageValidator,
+    productController.postproductImages
+);
 
 router.post("/deleteToken/:id", authController.removeAuthorization);
 
@@ -35,39 +35,39 @@ router.post("/auth/googleLogIn", authController.postGoogleLoginIn);
 
 router.post("/auth/login", authController.postLogin);
 
-// router.get("/getRatingPerStar/:query", productController.getRatingPerStar);
+router.get("/getRatingPerStar/:query", productController.getRatingPerStar);
 
-// router.get("/review", productController.getReview);
+router.get("/review", productController.getReview);
 
-// router.post("/paymentSuccessFull", productController.postPaymentSuccessFull);
+router.post("/paymentSuccessFull", productController.postPaymentSuccessFull);
 
-// router.post("/comment", productController.postComment);
+router.post("/comment", productController.postComment);
 
-// router.get("/shop", productController.getProducts);
+router.get("/shop", productController.getProducts);
 
-// router.get("/shop/detailes/:id", productController.getProductInfo);
+router.get("/shop/detailes/:id", productController.getProductInfo);
 
-// router.get("/getOrders", productController.getOrders);
+router.get("/getOrders", productController.getOrders);
 
-// router.post("/order/beforePayment", productController.postbeforePayment);
+router.post("/order/beforePayment", productController.postbeforePayment);
 
-// router.post("/cart/delete/:id", productController.deleteCartItem);
+router.post("/cart/delete/:id", productController.deleteCartItem);
 
-// router.post("/cart/order", productController.postOrder);
+router.post("/cart/order", productController.postOrder);
 
-// router.post("/cart/orderFailed", productController.postOrderFailed);
+router.post("/cart/orderFailed", productController.postOrderFailed);
 
-// router.get("/callback", productController.getCallBack);
+router.get("/callback", productController.getCallBack);
 
-// router.post("/order/razorpay", productController.postOrderRazorpay);
+router.post("/order/razorpay", productController.postOrderRazorpay);
 
-// router.post("/verification", productController.orderVerification);
+router.post("/verification", productController.orderVerification);
 
-// router.post("/callback", productController.postCallback);
+router.post("/callback", productController.postCallback);
 
-// router.post("/cart/:id", productController.addCartProduct);
+router.post("/cart/:id", productController.addCartProduct);
 
-// router.get("/cart/:id", productController.getCartProducts);
+router.get("/cart/:id", productController.getCartProducts);
 
 router.get("/", productController.welcome);
 
