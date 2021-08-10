2d5fd20f49249c1706c31aa401d3e0859deeed66
DeepeshDragoneel <deepeshash444@gmail.com>
2021-08-10 18:10:58 +0530

    Made it better

--- controllers/authController.js
+++ controllers/authController.js
@@ -196,7 +196,9 @@ exports.postSignUp = async (req, res, next) => {
                     to: req.body.data.email.trim(),
                     from: "emarting248@gmail.com",
                     subject: "Verify your account!",
-                    html: `<div><h1>Please Verify you Account!</h1><p>Click the Below link to verify your account</p><br/><br/><br/><a href=https://deepeshdragoneel.github.io/auth/verifySignUp/${userToken}>Verifiy Your Email</a></div>`,
+                    html: `<div><h1>Please Verify you Account!</h1><p>Click the Below link to verify your account</p><br/><br/><br/><a href=http://${
+                        (process.env, REST_URL)
+                    }/auth/verifySignUp/${userToken}>Verifiy Your Email</a></div>`,
                 });
                 console.log("Email res: ", res);
             } catch (error) {
